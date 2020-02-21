0be6d0d34508f44997686f8732aaa44567f3c14a
UkezekiHiroumi <go--ri--85810@softbank.ne.jp>
2020-02-21 19:47:14 +0900

    first commit

---
+++ tf_idf.py
@@ -0,0 +1,121 @@
+import os
+import re
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem.porter import PorterStemmer
+import nltk
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+
+df = pd.read_csv('movie_data.csv', encoding='utf-8')
+df = df[:1000]
+
+count = CountVectorizer()
+docs = np.array([
+        'The sun is shining',
+        'The weather is sweet',
+        'The sun is shining, the weather is sweet, and one and one is two'])
+bag = count.fit_transform(docs)
+print(count.vocabulary_)
+print(bag.toarray())
+
+np.set_printoptions(precision=2)
+
+tfidf = TfidfTransformer(use_idf=True, 
+                         norm='l2', 
+                         smooth_idf=True)
+print(tfidf.fit_transform(count.fit_transform(docs))
+      .toarray())
+
+tf_is = 3
+n_docs = 3
+idf_is = np.log((n_docs+1) / (3+1))
+tfidf_is = tf_is * (idf_is + 1)
+print('tf-idf of term "is" = %.2f' % tfidf_is)
+
+tfidf = TfidfTransformer(use_idf=True, norm=None, smooth_idf=True)
+raw_tfidf = tfidf.fit_transform(count.fit_transform(docs)).toarray()[-1]
+print(raw_tfidf)
+
+l2_tfidf = raw_tfidf / np.sqrt(np.sum(raw_tfidf**2))
+print(l2_tfidf)
+
+df.loc[0, 'review'][-50:]
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return text
+
+preprocessor(df.loc[0, 'review'][-50:])
+preprocessor("</a>This :) is :( a test :-)!")
+df['review'] = df['review'].apply(preprocessor)
+
+porter = PorterStemmer()
+
+def tokenizer(text):
+    return text.split()
+
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+nltk.download('all')
+
+stop = stopwords.words('english')
+
+X_train = df.loc[:700, 'review'].values
+y_train = df.loc[:700, 'sentiment'].values
+X_test = df.loc[700:, 'review'].values
+y_test = df.loc[700:, 'sentiment'].values
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf':[False],
+               'vect__norm':[None],
+               'clf__penalty': ['l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=1,
+                           n_jobs=-1)
+
+# if 'TRAVIS' in os.environ:
+#     gs_lr_tfidf.verbose=2
+#     X_train = df.loc[:250, 'review'].values
+#     y_train = df.loc[:250, 'sentiment'].values
+#     X_test = df.loc[25000:25250, 'review'].values
+#     y_test = df.loc[25000:25250, 'sentiment'].values
+
+gs_lr_tfidf.fit(X_train, y_train)
+
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
+
